dd8f49049342b84e8561292fb873aa36373c0712
Filip Sardzoski <filipsardzoski@Filips-MacBook-Air.local>
2017-04-17 12:39:28 +0200

    Implemented Camera functionality

---
+++ Domashna_141156/CameraViewController.swift
@@ -0,0 +1,57 @@
+//
+//  CameraViewController.swift
+//  Domashna_141156
+//
+//  Created by Filip Sardzoski on 4/17/17.
+//  Copyright © 2017 Filip Sardzoski. All rights reserved.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imagePicked: UIImageView!
+    let picker = UIImagePickerController()
+    
+    @IBAction func openCameraButton(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera) {
+            picker.allowsEditing = false
+            picker.sourceType = UIImagePickerControllerSourceType.camera
+            picker.cameraCaptureMode = .photo
+            picker.modalPresentationStyle = .fullScreen
+            present(picker, animated: true, completion: nil)
+        } else {
+            let alertVC = UIAlertController(
+                title: "No Camera",
+                message: "Sorry, this device has no camera",
+                preferredStyle: .alert)
+            let okAction = UIAlertAction(
+                title: "OK",
+                style:.default,
+                handler: nil)
+            alertVC.addAction(okAction)
+            present(
+                alertVC,
+                animated: true,
+                completion: nil)
+        }
+    }
+    //func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any])
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        var chosenImage = UIImage()
+        chosenImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        imagePicked.contentMode = .scaleAspectFit
+        imagePicked.image = chosenImage
+        dismiss(animated:true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        picker.delegate = self
+        // Do any additional setup after loading the view.
+    }
+}
